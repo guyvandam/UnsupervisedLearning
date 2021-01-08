1b0cb3f0b1b4108335451aa0e58d435d6cceaa4e
guyvandam <guyvandam@gmail.com>
2021-01-08 21:47:34 +0200

    Plots

--- Code/FitExternalClass.py
+++ Code/FitExternalClass.py
@@ -6,24 +6,28 @@ import pandas as pd
 import ClusteringAlgorithms
 import DataSets
 import GlobalParameters
-
+from DataSet1 import DataSet1
+from DataSet2 import DataSet2
+from DataSet3 import DataSet3
 
 class FitExternalClass():
 
-    def __init__(self, loadData, randomStates, clusteringAlgorithms=None):
-        self.loadData = loadData
+    def __init__(self, randomStateList=None, clusteringAlgorithms=None):
         self.clusteringAlgorithms =  ClusteringAlgorithms.clusteringAlgorithmsList if clusteringAlgorithms is None else clusteringAlgorithms
-        self.randomStates = randomStates
+        self.randomStateList = GlobalParameters.randomStates if randomStateList is None else randomStateList
 
-    def createCSV(self):
 
-        for label, content in self.loadData.getGroundTruth().iteritems():
+    
+
+    def createCSV(self, dataSet):
+        dataSet.prepareDataset()
+        for label, content in dataSet.getGroundTruth().iteritems():
             nClusters = len(set(content))
             print(f"Checking {label} Classifier With {nClusters} Clusters")
             result = {}
             for clusteringAlgorithm in self.clusteringAlgorithms:
-                clusteringAlgorithm.setDataFrame(self.loadData.getDataFrame())
-                result[clusteringAlgorithm.name] = clusteringAlgorithm.checkAgainstExternalClass(self.randomStates, content)
+                clusteringAlgorithm.setDataFrame(dataSet.getDataFrame())
+                result[clusteringAlgorithm.name] = clusteringAlgorithm.checkAgainstExternalClass(self.randomStateList, content)
             
             # ---------- Results into a DataFrame and add min and max columns ----------
             resultDF = pd.DataFrame(result)
@@ -33,7 +37,7 @@ class FitExternalClass():
             resultDF['Min'] = minList
             
             # ---------- Make Directory ----------
-            directory = os.path.join(os.getcwd(), f"Results\\Dataset{self.loadData.getDatasetIndex()}\\ExternalLabels")
+            directory = os.path.join(os.getcwd(), f"Results\\Dataset{dataSet.getDatasetIndex()}\\ExternalLabels")
             try:
                 os.makedirs(directory)
             except FileExistsError:
@@ -41,23 +45,22 @@ class FitExternalClass():
 
 
             # ---------- Save results in a CSV file ----------
-            resultDF.to_csv(directory+f"\\{label}ClassifierWith{nClusters}ClustersAnd{len(self.randomStates)}RandomStates.csv")
+            resultDF.to_csv(directory+f"\\{label}ClassifierWith{nClusters}ClustersAnd{len(self.randomStateList)}RandomStates.csv")
 
             # ---------- Save bar plot ----------
             algoNameAverageFitDict = {name:fitmentDict['Average'] for name, fitmentDict in result.items()}
             fig, ax = plt.subplots(figsize=(10, 8))
             ax.bar(list(algoNameAverageFitDict.keys()),algoNameAverageFitDict.values())
-            fig.suptitle(f"Average Of Kullback-Leibler Divergence Between Prediction Labels And External \n '{label}' Classifier With {nClusters} Clusters For Data-Set {self.loadData.getDatasetIndex()} Across {len(self.randomStates)} Random States")
-            fig.savefig(directory + f"\\{label}ClassifierWith{nClusters}ClustersAnd{len(self.randomStates)}RandomStates.png")
+            fig.suptitle(f"Average Of Kullback-Leibler Divergence Between Prediction Labels And External \n '{label}' Classifier With {nClusters} Clusters For Data-Set {dataSet.getDatasetIndex()} Across {len(self.randomStateList)} Random States")
+            fig.savefig(directory + f"\\{label}ClassifierWith{nClusters}ClustersAnd{len(self.randomStateList)}RandomStates.png")
             plt.close()
 
 
-
-# for ld in DataSets.dataSetList[0:1]:
-for ld in DataSets.dataSetList:
-
-    ld.prepareDataset()
-    print(f"Running On Dataset {ld.getDatasetIndex()}")
-    
-    fec = FitExternalClass(ld, GlobalParameters.randomStates)
-    fec.createCSV()
+"""
+Due to some memory interference issues with saving the figure and acessing the CSV file for loading the data, this function can't loop over the datasets and save their figures
+"""
+# ds = DataSet1()
+ds = DataSet2()
+# ds = DataSet3()
+fec = FitExternalClass()
+fec.createCSV(ds)
