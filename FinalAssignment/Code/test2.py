b30fb60a5c2c8301e277bbe0b42d7d12210dd3ba
guyvandam <guyvandam@gmail.com>
2021-03-17 23:31:56 +0200

    commit

--- FinalAssignment/Code/test2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-k = [1,2,3]
-g = [2,3,1]
-n_clusters_range = range(1, len(k)+1)
-plt.plot(n_clusters_range, k, 'o-', label="k")
-plt.plot(n_clusters_range, g, 'o-', label="g")
-plt.show()
